a8052a1d1c9eea1795dd12e30e22e51655619e4d
tanmaygolokait <tanmay@golokait.com>
2024-07-25 23:29:47 +0530

    Admin panel location changed

---
+++ src/Admin_Panel/Page/Members.tsx
@@ -0,0 +1,13 @@
+import Member_Cards from "../Page_Components/Members/Member_Cards";
+import Member_Table from "../Page_Components/Members/Member_Table";
+
+const Members = () => {
+  return (
+    <>
+      <Member_Cards />
+      <Member_Table />
+    </>
+  );
+};
+
+export default Members;
