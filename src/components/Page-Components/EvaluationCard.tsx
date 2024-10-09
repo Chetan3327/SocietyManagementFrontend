6b37cbbc936a6abe74137ac44c464139dca2efb7
Jyoti Ahuja <jyotiahuja822@gmail.com>
2024-10-09 16:23:15 +0530

    student profile changes

--- src/components/Page-Components/EvaluationCard.tsx
+++ src/components/Page-Components/EvaluationCard.tsx
@@ -5,38 +5,48 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Data } from "@/pages/Evaluation";
+import { format } from "date-fns";
 
-const EvaluationCard = ({ head, arr }: { head: string; arr: Data[] }) => {
+const EvaluationCard = ({ head, arr }: { head: string; arr: Data[] | string }) => {
+  
   return (
     <div className="flex justify-center flex-col items-center">
-      <h1 className="p-2 bg-blue-800 text-white px-2 mt-4 rounded-lg">
+      <h1 className="p-4 bg-blue-800 text-white  mt-4 rounded-lg text-lg">
         {head}
       </h1>
+
       <Card className="p-2 border-none">
         <CardContent className="p-2">
-          {arr.map(({ heading, date, points }, index: number) => {
-            return (
-              <CardDescription
-                key={index}
-                className="bg-blue-100 m-3 rounded-lg "
-              >
-                <CardTitle className="py-2 px-4 border border-b-2 border-b-black">
-                  <h1 className="text-black">{heading}</h1>
-                  <h6 className="text-sm">Duration: {date}</h6>
-                </CardTitle>
+          {typeof arr === "string" ? (
+            
+            <CardDescription className="bg-blue-100 m-3 rounded-lg">
+              <CardTitle className="py-6 px-4 ">
+                <h1 className="text-black">1. {arr}</h1>
+              </CardTitle>
+            </CardDescription>
+          ) : (
+            
+            arr.map(({ EnrollmentNo, Title, Description, DateAchieved }, index: number) => {
+              return (
+                <CardDescription
+                  key={index}
+                  className="bg-blue-100 m-3 rounded-lg"
+                >
+                  <CardTitle className="py-6 px-4 border border-b-2 border-b-black">
+                    <h1 className="text-black">{Title}</h1>
+                    <h6 className="text-sm">Duration: {format(DateAchieved,"MMMM dd, yyyy")}</h6>
+                  </CardTitle>
 
-                <ul className="list-disc list-outside py-2 px-6 font-medium text-black">
-                  {points.map((val: string, index: number) => {
-                    return (
-                      <li key={index} className="p-1">
-                        {val}
-                      </li>
-                    );
-                  })}
-                </ul>
-              </CardDescription>
-            );
-          })}
+                  <ul className="list-disc list-outside py-2 px-6 font-medium text-black">
+                    <li className="p-1">Enrollment No: {EnrollmentNo}</li>
+                    {/* <li className="p-1">Society ID: {SocietyID}</li>
+                    <li className="p-1">Achievement ID: {AchievementID}</li> */}
+                    <li className="p-1">Description: {Description}</li>
+                  </ul>
+                </CardDescription>
+              );
+            })
+          )}
         </CardContent>
       </Card>
     </div>
