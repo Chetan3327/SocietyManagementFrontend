2e1667fa5a43e4c75e64a58926a6ba10479a4118
sahilchauhan0603 <sahilchauhan0603@gmail.com>
2024-10-12 13:55:39 +0530

    loadbar

--- src/loadBar.tsx
+++ src/loadBar.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css'; // Import the nprogress CSS
+import './loadBar.css';
 
 const ProgressBar = () => {
   const location = useLocation();
