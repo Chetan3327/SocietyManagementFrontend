9ead0ec5ad7c531ddc9f9008cfe8c3cb2965b48a
sahilchauhan0603 <sahilchauhan0603@gmail.com>
2024-10-23 01:06:59 +0530

    barchart

--- src/Admin_Panel/Page/SocietyActivity.tsx
+++ src/Admin_Panel/Page/SocietyActivity.tsx
@@ -1,5 +1,5 @@
-import BarChart from '../Page_Components/Socities/BarChart'
-import SocietyPieChart from '../Page_Components/Socities/SocietyPieChart'
+// import BarChart from '../Page_Components/Socities/BarChart'
+// import SocietyPieChart from '../Page_Components/Socities/SocietyPieChart'
 import SocietyDetailsTable from '../Page_Components/Socities/SocietyDetailsTable'
 import Sidebar from '../Page_Components/Sidebar/Sidebar'
 import Header from '../Page_Components/Sidebar/Header'
@@ -12,14 +12,14 @@ const SocietyActivity = () => {
     <div className='flex gap-2 w-full overflow-x-hidden h-full.0.0.0.0.0.0.0.0.0.0'  >
       <Sidebar/>                       
     <div className='mt-10 '>
-        <div className='flex flex-col w-9/12 overflow-x-hidden lg:flex-row mx-5'>
+        {/* <div className='flex flex-col w-9/12 overflow-x-hidden lg:flex-row mx-5'>
             <BarChart />
             <div>
               <h2 className='text-center mb-4'>Activity Society</h2>
             <SocietyPieChart/>
             </div>
             
-        </div>
+        </div> */}
         <SocietyDetailsTable/>
     </div>
     </div>
